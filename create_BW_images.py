caea4433d4ee533b73a5678d8eff33d35bd157c6
atiyeashari <atiyeashari@gmail.com>
2018-02-03 22:13:21 -0500

    Created binary images

--- create_BW_images.py
+++ create_BW_images.py
@@ -16,11 +16,10 @@ def read_image():
         for image_path in glob.glob(os.path.join(folder_path, "*.png")):
             rgb_image = cv2.imread(image_path, cv2.IMREAD_COLOR)
             image_per_class[folder_name].append(rgb_image)
-            break
     return image_per_class
 
 
-def create_image_segment(image):
+def binary_image(image):
     # Convert BGR to HSV
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     # define range of green color in HSV
@@ -31,29 +30,19 @@ def create_image_segment(image):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
     # Remove the noise on plant's image
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(image, image, mask=mask)
-    return res
+    return mask
 
 
-def sharpen_image(image):
-    image_blurred = cv2.GaussianBlur(image, (0, 0), 3)
-    image_sharp = cv2.addWeighted(image, 1.5, image_blurred, -0.5, 0)
-    return image_sharp
-
-
-def test_sharpen_image():
+def save_image():
     images = read_image()
-    for image in images:
-        image_segment = create_image_segment(images[image][0])
-        sharpened_image = sharpen_image(image_segment)
-        fig, axs = plt.subplots(1, 3, figsize=(20, 20))
-        axs[0].imshow(images[image][0])
-        axs[1].imshow(image_segment)
-        axs[2].imshow(sharpened_image)
-        fig.savefig("tested_images/"+image+".png")
-
-# def save_image()
+    for image_class in images:
+        num = 0
+        if not os.path.exists('binary_images/'+image_class):
+            os.makedirs('binary_images/'+image_class)
+        for image in images[image_class]:
+            image_segment = binary_image(image)
+            np.save('binary_images/'+image_class+'/{0}.npy'.format(num), image_segment)
+            num += 1
 
 
-test_sharpen_image()
+save_image()
